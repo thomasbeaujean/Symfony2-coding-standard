8a6870b5ff0348678a1b688332ccac1a4e0cfb6b
Thomas Beaujean <t.beaujean@a5sys.com>
2015-11-06 16:13:41 +0100

    Add a warning on the use of private methods

---
+++ Symfony2/Sniffs/Scope/PrivateScopeSniff.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of the Symfony2-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   Thomas BEAUJEAN
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/escapestudios/Symfony2-coding-standard
+ */
+
+/**
+ * Symfony2_Sniffs_Constant_SelfSniff.
+ *
+ * Throws warnings if the self is used instead of static
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   Thomas BEAUJEAN
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony2-coding-standard
+ */
+class Symfony2_Sniffs_Scope_PrivateScopeSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+                                   'PHP',
+                                  );
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_PRIVATE);
+
+    }//end register()
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $phpcsFile->addWarning(
+            'Please use PROTECTED instead of PRIVATE, it is more flexible',
+            $stackPtr,
+            'Invalid'
+        );
+    }//end process()
+
+}//end class
+
